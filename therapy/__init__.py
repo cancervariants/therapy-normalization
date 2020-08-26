cc1a55183cc16a0856da43a388cd2ca0ded1f4da
Alex H. Wagner, PhD <Alex.Wagner@nationwidechildrens.org>
2020-08-26 09:39:12 -0400

    update dependencies and add logger

--- therapy/__init__.py
+++ therapy/__init__.py
@@ -1,4 +1,7 @@
 """The VICC library for normalizing therapies."""
 from pathlib import Path
+import logging
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
+logger = logging.getLogger('therapy')
+logger.setLevel(logging.DEBUG)
