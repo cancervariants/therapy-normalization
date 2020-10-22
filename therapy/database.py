cf222492f6e538cecb64080b1c567b0c87e99845
James Stevenson <james.stevenson@nationwidechildrens.org>
2020-10-22 15:20:19 -0400

    Update test set and testing formats

--- therapy/database.py
+++ therapy/database.py
@@ -3,11 +3,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from therapy import PROJECT_ROOT
+import os
 
-SQLALCHEMY_DATABASE_URL = f"sqlite:///{PROJECT_ROOT}/data/therapy.db"
+test = os.environ.get("THERAPY_DB_TEST")
+if not test:
+    uri = f"sqlite:///{PROJECT_ROOT}/data/therapy.db"
+elif test == "TEST":
+    uri = f"sqlite:///{PROJECT_ROOT}/tests/unit/data/test_therapy.db"
+
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    uri, connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
