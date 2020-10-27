814dc2045a67c6f9b7162b2cddf0370edfbfeaf3
James Stevenson <james.stevenson@nationwidechildrens.org>
2020-10-27 15:58:01 -0400

    Add names for constraints

--- therapy/database.py
+++ therapy/database.py
@@ -1,5 +1,5 @@
 """This module creates the database session."""
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from therapy import PROJECT_ROOT
@@ -17,4 +17,12 @@ engine = create_engine(
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+meta = MetaData(naming_convention=convention)
+Base = declarative_base(meta)
