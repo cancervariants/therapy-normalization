9d484254f05164f7df802ea292f38c2a96f3b7cc
James Stevenson <james.stevenson@nationwidechildrens.org>
2020-11-11 15:18:28 -0500

    Add skeleton of refactoring for dynamodb

--- therapy/etl/ncit.py
+++ therapy/etl/ncit.py
@@ -9,71 +9,42 @@ from sqlalchemy.orm import Session
 import owlready2 as owl
 from owlready2.entity import ThingClass
 from typing import Set
+import requests
+import zipfile
+import logging
+from os import remove, rename
+
+logger = logging.getLogger('therapy')
+logger.setLevel(logging.DEBUG)
 
 
 class NCIt(Base):
-    """Core NCIt ETL class
+    """Core NCIt ETL class.
+
     Extracting both:
      * NCIt classes with semantic_type "Pharmacologic Substance"
      * NCIt classes that are subclasses of C1909 (Pharmacologic Substance)
     """
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self,
+                 src_dirname="https://evs.nci.nih.gov/ftp1/NCI_Thesaurus/archive/20.09d_Release/",  # noqa F401
+                 src_fname="Thesaurus_20.09d.OWL.zip",
+                 *args,
+                 **kwargs):
         """Override base class init method. Call ETL methods with generated
         db SessionLocal instance.
         """
         B.metadata.create_all(bind=engine)
+        self._SRC_DIR = src_dirname
+        self._SRC_FNAME = src_fname
         self._extract_data()
         db: Session = SessionLocal()
         self._transform_data(db)
         db.commit()
         db.close()
 
-    def _load_data(self, db: Session, leaf: ThingClass):
+    def _load_data(self, therapy: Therapy):
         """Load data from individual NCIt entry into db"""
-        concept_id = f"{NamespacePrefix.NCIT.value}:{leaf.name}"
-        if leaf.P108:
-            label = leaf.P108.first()
-        else:
-            label = None
-        aliases = leaf.P90
-        if label and aliases and label in aliases:
-            aliases.remove(label)
-        therapy = Therapy(
-            concept_id=concept_id,
-            src_name=SourceName.NCIT.value,
-            label=label,
-        )
-        db.add(therapy)
-
-        if leaf.P207:
-            other_id = OtherIdentifier(
-                concept_id=concept_id,
-                other_id=f"{NamespacePrefix.UMLS.value}:{leaf.P207.first()}"
-            )
-            db.add(other_id)
-        if leaf.P210:
-            other_id = OtherIdentifier(
-                concept_id=concept_id,
-                other_id=f"{NamespacePrefix.CASREGISTRY.value}:{leaf.P210.first()}"  # noqa F501
-            )
-            db.add(other_id)
-        if leaf.P319:
-            other_id = OtherIdentifier(
-                concept_id=concept_id,
-                other_id=f"{NamespacePrefix.FDA.value}:{leaf.P319.first()}"
-            )
-            db.add(other_id)
-        if leaf.P320:
-            other_id = OtherIdentifier(
-                concept_id=concept_id,
-                other_id=f"{NamespacePrefix.ISO.value}:{leaf.P320.first()}"
-            )
-            db.add(other_id)
-
-        for a in aliases:
-            alias = Alias(alias=a, concept_id=concept_id)
-            db.add(alias)
 
     def get_desc_nodes(self, node: ThingClass,
                        uq_nodes: Set[ThingClass]) -> Set[ThingClass]:
@@ -123,8 +94,8 @@ class NCIt(Base):
                            data_license="CC BY 4.0",
                            data_license_url="https://creativecommons.org/licenses/by/4.0/legalcode",  # noqa F401
                            version=self._version,
-                           data_url="https://evs.nci.nih.gov/ftp1/NCI_Thesaurus/archive/20.09d_Release/",)  # noqa F401
-        db.add(meta_object)
+                           data_url=self._SRC_DIR)
+        db.add(meta_object)  # TODO replace w/ boto3 load()
 
     def _transform_data(self, db: Session, *args, **kwargs):
         """Get data from file and construct objects for loading"""
@@ -135,7 +106,67 @@ class NCIt(Base):
         uq_nodes = self.get_desc_nodes(ncit.C1909, uq_nodes)
         uq_nodes = self.get_typed_nodes(uq_nodes, ncit)
         for node in uq_nodes:
-            self._load_data(db, node)
+            concept_id = f"{NamespacePrefix.NCIT.value}:{node.name}"
+            if node.P108:
+                label = node.P108.first()
+            else:
+                label = None
+            aliases = [Alias()]
+            node.P90
+            if label and aliases and label in aliases:
+                aliases.remove(label)
+
+            other_ids = []
+            if node.P207:
+                other_id = OtherIdentifier(
+                    concept_id=concept_id,
+                    other_id=f"{NamespacePrefix.UMLS.value}:{node.P207.first()}"  # noqa F501
+                )
+                other_ids.append(other_id)
+            if node.P210:
+                other_id = OtherIdentifier(
+                    concept_id=concept_id,
+                    other_id=f"{NamespacePrefix.CASREGISTRY.value}:{node.P210.first()}"  # noqa F501
+                )
+                other_ids.append(other_id)
+            if node.P319:
+                other_id = OtherIdentifier(
+                    concept_id=concept_id,
+                    other_id=f"{NamespacePrefix.FDA.value}:{node.P319.first()}"
+                )
+                other_ids.append(other_id)
+            if node.P320:
+                other_id = OtherIdentifier(
+                    concept_id=concept_id,
+                    other_id=f"{NamespacePrefix.ISO.value}:{node.P320.first()}"
+                )
+                other_ids.append(other_id)
+
+            therapy = Therapy(concept_id=concept_id,  # noqa F841
+                              src_name=SourceName.NCIT.value,
+                              label=label,
+                              aliases=aliases,
+                              other_identifiers=other_ids)
+
+            # TODO call load() method
+
+    def _download_data(self):
+        logger.info('Downloading NCI Thesaurus...')
+        url = self._SRC_DIR + self._SRC_FNAME
+        out_dir = PROJECT_ROOT / 'data' / 'ncit'
+        zip_path = out_dir / 'ncit.zip'
+        response = requests.get(url, stream=True)
+        handle = open(zip_path, "wb")
+        for chunk in response.iter_content(chunk_size=512):
+            if chunk:
+                handle.write(chunk)
+        print(zip_path)
+        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+            zip_ref.extractall(out_dir)
+        remove(zip_path)
+        version = self._SRC_DIR.split('/')[-2].split('_')[0]
+        rename(out_dir / 'Thesaurus.owl', out_dir / f'ncit_{version}.owl')
+        logger.info('Finished downloading NCI Thesaurus')
 
     def _extract_data(self, *args, **kwargs):
         """Get NCIt source file"""
@@ -144,8 +175,9 @@ class NCIt(Base):
         else:
             data_dir = PROJECT_ROOT / 'data' / 'ncit'
             data_dir.mkdir(exist_ok=True, parents=True)
-            try:
-                self._data_src = sorted(list(data_dir.iterdir()))[-1]
-            except IndexError:
-                raise FileNotFoundError  # TODO download function here
+            dir_files = list(data_dir.iterdir())
+            if len(dir_files) == 0:
+                self._download_data()
+                dir_files = list(data_dir.iterdir())
+            self._data_src = sorted(dir_files)[-1]
         self._version = self._data_src.stem.split('_')[1]
