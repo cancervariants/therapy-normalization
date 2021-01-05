53726514619d92b99251bad5e5435730c74e2b33
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-01-05 11:54:11 -0500

    Fix test meta access

--- tests/unit/test_chembl_source.py
+++ tests/unit/test_chembl_source.py
@@ -461,7 +461,7 @@ def test_meta_info(cisplatin, chembl):
     assert normalizer_response['meta_'].version == '27'
     assert normalizer_response['meta_'].data_url == \
            'http://ftp.ebi.ac.uk/pub/databases/chembl/ChEMBLdb/releases/chembl_27/'  # noqa: E501
-    assert normalizer_response.rdp_url == 'http://reusabledata.org/chembl.html'
-    assert not normalizer_response.non_commercial
-    assert normalizer_response.share_alike
-    assert normalizer_response.attribution
+    assert normalizer_response['meta_'].rdp_url == 'http://reusabledata.org/chembl.html'  # noqa: E501
+    assert not normalizer_response['meta_'].non_commercial
+    assert normalizer_response['meta_'].share_alike
+    assert normalizer_response['meta_'].attribution
