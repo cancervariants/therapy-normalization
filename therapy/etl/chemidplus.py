ca7814332deb2a515fcb7c894c55e860af911ec7
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-01-07 09:14:49 -0500

    Add meta method

--- therapy/etl/chemidplus.py
+++ therapy/etl/chemidplus.py
@@ -2,7 +2,10 @@
 from .base import Base
 from therapy import PROJECT_ROOT
 from therapy.database import Database
+from therapy.schemas import Drug, NamespacePrefix, Meta, SourceName, \
+    DataLicenseAttributes
 from pathlib import Path
+import xml.etree.ElementTree as ET
 
 
 class ChemIDplus(Base):
@@ -30,6 +33,10 @@ class ChemIDplus(Base):
         raise NotImplementedError
 
     def _extract_data(self, data_dir):
+        """Acquire ChemIDplus dataset.
+
+        :arg pathlib.Path data_dir: directory containing source data
+        """
         data_dir.mkdir(exist_ok=True, parents=True)
         dir_files = list(data_dir.iterdir())
         if len(dir_files) == 0:
@@ -37,3 +44,44 @@ class ChemIDplus(Base):
             dir_files = list(data_dir.iterdir())
         self._data_src = sorted(dir_files)[-1]
         self._version = self._data_src.stem.split('_')[1]
+
+    def _transform_data(self):
+        """Open dataset and prepare for loading into database."""
+        tree = ET.parse(self._data_src)
+        root = tree.getroot()
+        with self.database.therapies.batch_writer() as batch:
+
+            for chemical in root:
+
+                name_element = chemical.find('NameList')
+                label = name_element.get('NameOfSubstance').text
+                aliases = [e.text for e in name_element if e.tag == 'Synonyms']
+                id_element = chemical.find('NumberList')
+                concept_id = f'{NamespacePrefix.CASREGISTRY}:{id_element.find("CASRegistryNumber").text}'  # noqa: E501
+
+                record = Drug(concept_id=concept_id,
+                              aliases=aliases,
+                              label=label,
+                              other_identifiers=[],
+                              xrefs=[])
+                self._load_record(batch, record)
+
+    def _load_record(self, batch, record: Drug):
+        """Load individual record into database."""
+        pass
+
+    def _load_meta(self):
+        """Add source metadata."""
+        meta = Meta(data_license="custom",
+                    data_license_url="https://www.nlm.nih.gov/databases/download/terms_and_conditions.html",  # noqa: E501
+                    version="20200107",
+                    data_url="ftp://ftp.nlm.nih.gov/nlmdata/.chemidlease/",
+                    rdp_url="",
+                    data_license_attributes=DataLicenseAttributes(
+                        non_commercial=False,
+                        share_alike=False,
+                        attribution=True
+                    ))
+        item = dict(meta)
+        item['src_name'] = SourceName.CHEMIDPLUS
+        self.database.metadata.put_item(Item=item)
