7f333632f20e42c6e1e960b67acacdb540a34b60
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-01-12 16:47:03 -0500

    Add trade names

--- therapy/etl/chemidplus.py
+++ therapy/etl/chemidplus.py
@@ -8,6 +8,7 @@ from pathlib import Path
 import xml.etree.ElementTree as ET
 import requests
 import logging
+from botocore.exceptions import ClientError
 
 
 logger = logging.getLogger('therapy')
@@ -32,8 +33,10 @@ class ChemIDplus(Base):
         self.database = database
         self._src_dir = src_dir
         self._src_fname = src_fname
+        self._added_ids = set()
         self._extract_data(data_path)
         self._transform_data()
+        self._add_meta()
 
     def _download_data(self):
         logger.info('Downloading ChemIDplus data...')
@@ -67,57 +70,91 @@ class ChemIDplus(Base):
         root = tree.getroot()
         with self.database.therapies.batch_writer() as batch:
             for chemical in root:
+                # get names
                 name_element = chemical.find('NameList')
-                label = name_element.get('NameOfSubstance').text
+                if name_element.find('NameOfSubstance'):
+                    label = name_element.find('NameOfSubstance').text
+                elif name_element.find('SystematicName'):
+                    label = name_element.find('SystematicName').text
+                trade_names = []
+                mixture_names = name_element.findall('MixtureName')
+                if mixture_names:
+                    trade_names = [n.text for n in mixture_names]
                 alias_elms = [e for e in name_element if e.tag == 'Synonyms']
                 if len(alias_elms) > 20:
                     aliases = []
                 else:
                     aliases = [e.text for e in alias_elms]
+                # get concept ID
                 id_list = chemical.find('NumberList')
-                concept_id = f'{NamespacePrefix.CASREGISTRY}:{id_list.find("CASRegistryNumber").text}'  # noqa: E501
+                chemid_num = id_list.find("CASRegistryNumber").text
+                if not chemid_num:
+                    continue
+                concept_id = f'{NamespacePrefix.CASREGISTRY.value}:{chemid_num}'  # noqa: E501
+                # get other_ids and xrefs
                 other_identifiers = []
                 xrefs = []
                 locator_list = chemical.find('LocatorList')
                 for loc in locator_list:
-                    url = loc.attrib['url']
-                    if 'drugbank' in url.lower():
-                        db = f'{NamespacePrefix.DRUGBANK}:{url.split("/")[-1]}'
-                        other_identifiers.append(db)
-                    elif 'fdasis' in url.lower():
-                        fda = f'{NamespacePrefix.FDA}:{url.split("/")[-1]}'
-                        xrefs.apppend(fda)
+                    if 'url' in loc.attrib:
+                        url = loc.attrib['url']
+                        if 'drugbank' in url.lower():
+                            db = f'{NamespacePrefix.DRUGBANK.value}:{url.split("/")[-1]}'  # noqa: E501
+                            other_identifiers.append(db)
+                        elif 'fdasis' in url.lower():
+                            fda = f'{NamespacePrefix.FDA.value}:{url.split("/")[-1]}'  # noqa: E501
+                            xrefs.append(fda)
+                # load completed record
                 record = Drug(concept_id=concept_id,
                               aliases=aliases,
+                              trade_names=trade_names,
                               label=label,
                               other_identifiers=other_identifiers,
                               xrefs=xrefs)
-                self._load_record(batch, record)
+                if record.concept_id not in self._added_ids:
+                    try:
+                        self._load_record(batch, record)
+                        self._added_ids.add(record.concept_id)
+                    except ClientError:
+                        print(record)
+                        print(record.concept_id in self._added_ids)
+                else:
+                    print(record)
 
     def _load_record(self, batch, record: Drug):
-        """Load individual record into database."""
+        """Load individual record into database.
+
+        :param batch: dynamodb batch writer
+        :param therapy.schemas.Drug record: complete drug record to upload
+        """
         for alias in record.aliases:
             batch.put_item(Item={
                 'label_and_type': f'{alias}##alias',
                 'concept_id': f'{record.concept_id.lower()}',
-                'src_name': SourceName.CHEMIDPLUS
+                'src_name': SourceName.CHEMIDPLUS.value
+            })
+        for trade_name in record.trade_names:
+            batch.put_item(Item={
+                'label_and_type': f'{trade_name}##trade_name',
+                'concept_id': f'{record.concept_id.lower()}',
+                'src_name': SourceName.CHEMIDPLUS.value
             })
         if record.label:
             batch.put_item(Item={
                 'label_and_type': f'{record.label}',
                 'concept_id': f'{record.concept_id.lower()}',
-                'src_name': SourceName.CHEMIDPLUS
+                'src_name': SourceName.CHEMIDPLUS.value
             })
         id_record = dict(record)
-        id_record['src_name'] = SourceName.CHEMIDPLUS
+        id_record['src_name'] = SourceName.CHEMIDPLUS.value
         id_record['label_and_type'] = f'{record.concept_id.lower()}##identity'
         batch.put_item(Item=id_record)
 
-    def _load_meta(self):
+    def _add_meta(self):
         """Add source metadata."""
         meta = Meta(data_license="custom",
                     data_license_url="https://www.nlm.nih.gov/databases/download/terms_and_conditions.html",  # noqa: E501
-                    version="20200107",
+                    version=self._version,
                     data_url="ftp://ftp.nlm.nih.gov/nlmdata/.chemidlease/",
                     rdp_url="",
                     data_license_attributes=DataLicenseAttributes(
@@ -126,5 +163,5 @@ class ChemIDplus(Base):
                         attribution=True
                     ))
         item = dict(meta)
-        item['src_name'] = SourceName.CHEMIDPLUS
+        item['src_name'] = SourceName.CHEMIDPLUS.value
         self.database.metadata.put_item(Item=item)
