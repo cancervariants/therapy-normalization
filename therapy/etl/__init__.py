6f43bb404d66b3b2e6bd47509c5959165c82ba92
korikuzma <korikuzma@gmail.com>
2021-01-12 16:00:54 -0500

    WIP: Implement RxNorm Normalizer

--- therapy/etl/__init__.py
+++ therapy/etl/__init__.py
@@ -3,3 +3,4 @@ from .wikidata import Wikidata  # noqa: F401
 from .chembl import ChEMBL  # noqa: F401
 from .ncit import NCIt  # noqa: F401
 from .drugbank import DrugBank  # noqa: F401
+from .rxnorm import RxNorm  # noqa: F401
