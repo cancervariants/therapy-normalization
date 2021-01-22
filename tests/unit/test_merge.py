43fc439e0ffcd9a1eabfbe0b7d9f6407ec989ebd
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-01-22 12:05:06 -0500

    Add quote unquote mock db

--- tests/unit/test_merge.py
+++ tests/unit/test_merge.py
@@ -2,7 +2,50 @@
 import pytest
 from therapy.etl.merge import Merge
 from therapy.database import Database
-from typing import Dict
+from therapy import PROJECT_ROOT
+from typing import Dict, Any, Optional
+import json
+
+
+class MockDatabase(Database):
+    """`Mock` database object to use in test cases."""
+
+    def __init__(self):
+        """Initialize mock database object.
+
+        self.records contains
+        """
+        infile = PROJECT_ROOT / 'tests' / 'unit' / 'data' / 'therapies.json'
+        with open(infile, 'r') as f:
+            self.records = json.load(f)
+        self.updates = {}
+
+    def get_record_by_id(self, record_id: str) -> Optional[Dict]:
+        """Fetch record corresponding to provided concept ID.
+
+        :param str concept_id: concept ID for therapy record
+        :param bool case_sensitive: if true, performs exact lookup, which is
+            more efficient. Otherwise, performs filter operation, which
+            doesn't require correct casing.
+        :return: complete therapy record, if match is found; None otherwise
+        """
+        key = f'{record_id.lower()}##identity'
+        for record in self.records:
+            if record['label_and_type'] == key:
+                return record
+        return None
+
+    def update_record(self, concept_id: str, attribute: str, value: Any):
+        """Store update request sent to database.
+
+        :param str concept_id: record to update
+        :param str field: name of field to update
+        :parm str value: new value
+        """
+        self.updates[concept_id] = {
+            'concept_id': concept_id,
+            attribute: value
+        }
 
 
 @pytest.fixture(scope='module')
@@ -10,7 +53,7 @@ def merge_handler():
     """Provide Merge instance to test cases."""
     class MergeHandler():
         def __init__(self):
-            self.merge = Merge(Database())
+            self.merge = Merge(MockDatabase())
 
         def get_merge(self):
             return self.merge
