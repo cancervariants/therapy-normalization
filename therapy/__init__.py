5012e25e38364bd57b0d48c4217fd845054c739d
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-03-24 13:55:02 -0400

    Update version

--- therapy/__init__.py
+++ therapy/__init__.py
@@ -10,7 +10,7 @@ logger = logging.getLogger('therapy')
 logger.setLevel(logging.DEBUG)
 
 # TODO: Fix so that we don't have to change in setup.cfg
-__version__ = "0.2.8"
+__version__ = "0.2.9"
 
 
 class DownloadException(Exception):
