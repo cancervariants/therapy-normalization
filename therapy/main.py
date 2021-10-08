f82b14027f433a9a7b2103ea0e17f811b5de7f13
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-10-08 16:49:38 -0400

    Testing import order check

--- therapy/main.py
+++ therapy/main.py
@@ -1,10 +1,10 @@
 """Main application for FastAPI"""
+import html
 from therapy import __version__
 from therapy.query import QueryHandler, InvalidParameterException
 from therapy.schemas import SearchService, NormalizationService
 from fastapi import FastAPI, HTTPException, Query
 from fastapi.openapi.utils import get_openapi
-import html
 from typing import Optional
 
 query_handler = QueryHandler()
