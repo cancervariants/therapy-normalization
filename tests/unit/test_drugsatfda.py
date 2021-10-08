5f4988894219898ad1651e592f1809ee53ff39ed
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-10-08 15:54:06 -0400

    Add drugs@fda progress

--- tests/unit/test_drugsatfda.py
+++ tests/unit/test_drugsatfda.py
@@ -1,1 +1,193 @@
 """Test correctness of Drugs@FDA ETL methods."""
+from datetime import datetime
+from typing import Dict, List
+
+import pytest
+
+from tests.conftest import compare_records
+from therapy.query import QueryHandler
+from therapy.schemas import MatchType
+
+
+@pytest.fixture(scope="module")
+def drugsatfda():
+    """Build Drugs@FDA test fixture."""
+    class QueryGetter:
+
+        def __init__(self):
+            self.normalizer = QueryHandler()
+
+        def search(self, query_str):
+            resp = self.normalizer.search_sources(query_str, keyed=True,
+                                                  incl="drugsatfda")
+            return resp["source_matches"]["DrugsAtFDA"]
+    return QueryGetter()
+
+
+@pytest.fixture(scope="module")
+def everolimus() -> Dict:
+    """Build afinitor test fixture."""
+    return {
+        "label": "EVEROLIMUS",
+        "concept_id": "drugsatfda:NDA022334",
+        "aliases": [],
+        "xrefs": [
+            "rxcui:998191",
+            "rxcui:1310144",
+            "rxcui:1310147",
+            "rxcui:845507",
+            "rxcui:845518",
+            "rxcui:845515",
+            "rxcui:1310138",
+            "rxcui:845512",
+            "rxcui:998189",
+            "rxcui:1308430",
+            "rxcui:1119402",
+            "rxcui:1308428",
+            "rxcui:1119400",
+            "rxcui:1308432"
+        ],
+        "associated_with": ["unii:9HW64Q8G6G"],
+        "approval_status": "fda_prescription",
+        "trade_names": ["AFINITOR"]
+    }
+
+
+@pytest.fixture(scope="module")
+def cosmegen() -> Dict:
+    """Build cosmegen test fixture."""
+    return {
+        "label": "DACTINOMYCIN",
+        "concept_id": "drugsatfda:NDA050682",
+        "xrefs": [
+            "rxcui:105569",
+            "rxcui:239179"
+        ],
+        "associated_with": ["unii:1CC1JFE158"],
+        "approval_status": "fda_prescription",
+        "trade_names": ["COSMEGEN"]
+    }
+
+
+@pytest.fixture(scope="module")
+def cisplatin() -> List[Dict]:
+    """Build cisplatin test fixtures"""
+    return [
+        {
+            "label": "CISPLATIN",
+            "concept_id": "drugsatfda:ANDA074656",
+            "xrefs": ["rxcui:309311"],
+            "associated_with": ["unii:Q20Q21Q62J"],
+            "approval_status": "fda_prescription",
+        },
+        {
+            "label": "CISPLATIN",
+            "concept_id": "drugsatfda:ANDA075036",
+            "xrefs": ["rxcui:309311"],
+            "associated_with": ["unii:Q20Q21Q62J"],
+            "approval_status": "fda_prescription",
+        },
+        {
+            "label": "CISPLATIN",
+            "concept_id": "drugsatfda:ANDA074735",
+            "xrefs": ["rxcui:309311"],
+            "associated_with": ["unii:Q20Q21Q62J"],
+            "approval_status": "fda_prescription",
+        },
+        {
+            "label": "CISPLATIN",
+            "concept_id": "drugsatfda:ANDA206774",
+            "xrefs": ["rxcui:309311"],
+            "associated_with": ["unii:Q20Q21Q62J"],
+            "approval_status": "fda_prescription",
+        },
+        {
+            "label": "CISPLATIN",
+            "concept_id": "drugsatfda:ANDA207323",
+            "aliases": [],
+            "xrefs": ["rxcui:309311"],
+            "associated_with": ["unii:Q20Q21Q62J"],
+            "approval_status": "fda_prescription",
+        }
+    ]
+
+
+def test_everolimus(drugsatfda, everolimus):
+    """Test everolimus/afinitor"""
+    # test concept ID
+    response = drugsatfda.search("drugsatfda:NDA022334")
+    assert response["match_type"] == MatchType.CONCEPT_ID
+    assert len(response["records"]) == 1
+    compare_records(response[0], everolimus)
+
+    response = drugsatfda.search("drugsatfda:ANDA022334")
+    assert response["match_type"] == MatchType.NO_MATCH
+
+    # test label
+    response = drugsatfda.search("everolimus")
+    assert response["match_type"] == MatchType.CONCEPT_ID
+    assert len(response["records"]) == 1
+    compare_records(response[0], everolimus)
+
+    # test trade name
+    response = drugsatfda.search('afinitor')
+    assert response["match_type"] == MatchType.TRADE_NAME
+    assert len(response["records"]) == 1
+    compare_records(response[0], everolimus)
+
+    # test xref
+    response = drugsatfda.search('rxcui:998191')
+    assert response["match_type"] == MatchType.XREF
+    assert len(response["records"]) == 1
+    compare_records(response[0], everolimus)
+
+    response = drugsatfda.search('rxcui:845507')
+    assert response["match_type"] == MatchType.XREF
+    assert len(response["records"]) == 1
+    compare_records(response[0], everolimus)
+
+    response = drugsatfda.search('rxcui:998189')
+    assert response["match_type"] == MatchType.XREF
+    assert len(response["records"]) == 1
+    compare_records(response[0], everolimus)
+
+    response = drugsatfda.search('rxcui:1308432')
+    assert response["match_type"] == MatchType.XREF
+    assert len(response["records"]) == 1
+    compare_records(response[0], everolimus)
+
+
+def test_cosmegen(drugsatfda, cosmegen):
+    """Test cosmegen"""
+    # test concept ID
+    response = drugsatfda.search("drugsatfda:NDA050682")
+    assert response["match_type"] == MatchType.CONCEPT_ID
+    assert len(response["records"]) == 1
+    compare_records(response[0], cosmegen)
+
+    response = drugsatfda.search("drugsatfda:ANDA050682")
+
+
+def test_other_parameters(drugsatfda):
+    """Test other design decisions that aren't captured in the defined
+    fixtures.
+    """
+    # test dropping BLA records
+    response = drugsatfda.search("drugsatfda:BLA020725")
+    assert response["match_type"] == MatchType.NO_MATCH
+
+
+def test_meta(drugsatfda):
+    """Test correctness of source metadata."""
+    response = drugsatfda.search("incoherent-string-of-text")
+    assert response["source_meta_"]["data_license"] == "CC0"
+    assert response["source_meta_"]["data_license_url"] == "https://creativecommons.org/publicdomain/zero/1.0/legalcode"  # noqa: E501
+    version = response["source_meta_"]["version"]
+    assert datetime.strptime(version, "%Y%m%d")
+    assert response["source_meta_"]["data_url"] == "https://open.fda.gov/apis/drug/drugsfda/download/"  # noqa: E501
+    assert response["source_meta_"]["rdp_url"] is None
+    assert response["source_meta_"]["data_license_attributes"] == {
+        "non_commercial": False,
+        "share_alike": False,
+        "attribution": False,
+    }
--- therapy/etl/drugsatfda.py
+++ therapy/etl/drugsatfda.py
@@ -1,5 +1,5 @@
 """ETL methods for the Drugs@FDA source."""
-from typing import List, Union, Dict
+from typing import List, Union, Dict, Optional
 import zipfile
 from io import BytesIO
 import json
@@ -67,32 +67,48 @@ class DrugsAtFDA(Base):
         meta["src_name"] = SourceName.DRUGSATFDA
         self.database.metadata.put_item(Item=meta)
 
-    def _transform_data(self):
-        """Prepare source data for loading into DB."""
-        with open(self._src_file, "r") as f:
-            data = json.load(f)["results"]
-
+    def _get_marketing_status(self, products: List, concept_id: str)\
+            -> Optional[str]:
+        """Get approval status from products list.
+        :param List products: list of individual FDA product objects
+        :param str concept_id: FDA application concept ID, used in reporting
+            error messages
+        :return: approval_status value if successful, None if ambiguous or
+            unavailable
+        """
         statuses_map = {
             "Discontinued": ApprovalStatus.FDA_DISCONTINUED.value,
             "Prescription": ApprovalStatus.FDA_PRESCRIPTION.value,
             "Over-the-counter": ApprovalStatus.FDA_OTC.value,
             "None (Tentative Approval)": ApprovalStatus.FDA_TENTATIVE.value,
         }
+        statuses = [p["marketing_status"] for p in products]
+        if not all([s == statuses[0] for s in statuses]):
+            msg = (f"Application {concept_id} has inconsistent marketing "
+                   f"statuses: {statuses}")
+            logger.info(msg)
+            return None
+        else:
+            return statuses_map.get(statuses[0])
+
+    def _transform_data(self):
+        """Prepare source data for loading into DB."""
+        with open(self._src_file, "r") as f:
+            data = json.load(f)["results"]
 
         for result in data:
-            concept_id = f"{NamespacePrefix.DRUGSATFDA.value}:{result['application_number']}"  # noqa: E501
-            therapy: Dict[str, Union[str, List]] = {"concept_id": concept_id}
             if "products" not in result:
                 continue
             products = result["products"]
 
-            statuses = [p["marketing_status"] for p in products]
-            if not all([s == statuses[0] for s in statuses]):
-                msg = (f"Application {concept_id} has inconsistent marketing "
-                       f"statuses: {statuses}")
-                logger.info(msg)
+            app_no = result["application_number"]
+            if app_no.startswith("BLA"):
+                # ignore biologics license applications (tentative)
                 continue
-            status = statuses_map.get(statuses[0])
+            concept_id = f"{NamespacePrefix.DRUGSATFDA.value}:{app_no}"
+            therapy: Dict[str, Union[str, List]] = {"concept_id": concept_id}
+
+            status = self._get_marketing_status(products, concept_id)
             if status:
                 therapy["approval_status"] = status
 
@@ -106,25 +122,27 @@ class DrugsAtFDA(Base):
                 if brand_name:
                     brand_names += brand_name
 
-                substance = openfda.get("substance_name", [])
-                num_substances = len(substance)
-                if num_substances > 1:
-                    msg = (f"Application {concept_id} has >1 substance names: "
-                           f"{substance}")
-                    logger.info(msg)
-                elif num_substances == 1:
-                    therapy["label"] = substance[0]
+                substances = openfda.get("substance_name", [])
+                n_substances = len(set(substances))
+                if n_substances > 1:
+                    # if ambiguous, store all as aliases
+                    msg = (f"Application {concept_id} has {n_substances} "
+                           f"substance names: {substances}")
+                    logger.debug(msg)
+                    aliases += substances
+                elif n_substances == 1:
+                    therapy["label"] = substances[0]
 
-                generic = openfda.get("generic_name", [])
-                if len(generic) > 1:
-                    msg = (f"Application {concept_id} has >1 generic names: "
-                           f"{generic}")
-                    logger.info(msg)
-                elif len(generic) == 1:
-                    if num_substances == 0:
-                        therapy["label"] = generic[0]
+                generics = openfda.get("generic_name", [])
+                n_generic = len(set(generics))
+                if n_generic > 1:
+                    aliases += generics
+                elif n_generic == 1:
+                    if n_substances == 0:
+                        # there are about 300 cases of this
+                        therapy["label"] = generics[0]
                     else:
-                        aliases.append(generic[0])
+                        aliases.append(generics[0])
 
                 unii = openfda.get("unii")
                 if unii:
