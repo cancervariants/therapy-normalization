15287a1e04812b0292b2b843f6b0fa0a8cbcb696
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-10-18 14:56:46 -0400

    Fix therapy vs drug discrepancy

--- therapy/etl/ncit.py
+++ therapy/etl/ncit.py
@@ -11,7 +11,7 @@ from owlready2.entity import ThingClass
 
 from therapy import PROJECT_ROOT
 from therapy.database import Database
-from therapy.schemas import SourceName, NamespacePrefix, Therapy, SourceMeta
+from therapy.schemas import SourceName, NamespacePrefix, SourceMeta
 from therapy.etl.base import Base
 
 logger = logging.getLogger("therapy")
@@ -162,7 +162,6 @@ class NCIt(Base):
                 "xrefs": xrefs,
                 "associated_with": associated_with
             }
-            assert Therapy(**params)
             self._load_therapy(params)
 
     def _load_meta(self) -> None:
