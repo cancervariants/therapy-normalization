65b1873d9873e74fa60591cdb425be7eb34379e3
James Stevenson <james.stevenson@nationwidechildrens.org>
2021-11-24 13:18:32 -0500

    Fix drugs@fda namespace bug

--- therapy/etl/merge.py
+++ therapy/etl/merge.py
@@ -160,7 +160,7 @@ class Merge:
         """
         if record_id in self._groups:
             return self._groups[record_id]
-        elif record_id.startswith("drugsatfda:"):
+        elif record_id.startswith("drugsatfda"):
             return {record_id}
         elif record_id in self._failed_lookups:
             return observed_id_set - {record_id}
