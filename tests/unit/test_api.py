96bf74ef637f42cadd3fc82502218217452ba3c2
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-07-02 07:17:47 -0400

    fix tests more

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -11,9 +11,10 @@ from datetime import datetime
 import pytest
 import pytest_asyncio
 from asgi_lifespan import LifespanManager
-from disease.main import app
-from disease.schemas import ServiceEnvironment
 from httpx import ASGITransport, AsyncClient
+
+from therapy.main import app
+from therapy.schemas import ServiceEnvironment
 
 
 @pytest_asyncio.fixture
@@ -33,28 +34,32 @@ async def api_client(async_app):
 @pytest.mark.asyncio
 async def test_search(api_client: AsyncClient):
     """Test /search endpoint."""
-    response = await api_client.get("/disease/search?q=neuroblastoma")
+    response = await api_client.get("/therapy/search?q=trastuzumab")
     assert response.status_code == 200
     assert (
-        response.json()["source_matches"]["Mondo"]["records"][0]["concept_id"]
-        == "mondo:0005072"
+        response.json()["source_matches"]["NCIt"]["records"][0]["concept_id"]
+        == "ncit:C1647"
     )
 
-    response = await api_client.get("/disease/search?q=neuroblastoma&incl=sdkl")
+    response = await api_client.get("/therapy/search?q=trastuzumab&incl=sdkl")
     assert response.status_code == 422
 
-    response = await api_client.get("/disease/search")
+    response = await api_client.get("/therapy/search")
     assert response.status_code == 422
 
 
 @pytest.mark.asyncio
 async def test_normalize(api_client: AsyncClient):
     """Test /normalize endpoint."""
-    response = await api_client.get("/disease/normalize?q=neuroblastoma")
+    response = await api_client.get("/therapy/normalize?q=cisplatin")
     assert response.status_code == 200
-    assert response.json()["disease"]["primaryCode"] == "ncit:C3270"
+    assert response.json()["therapy"]["primaryCoding"] == {
+        "code": "2555",
+        "id": "rxcui:2555",
+        "system": "https://mor.nlm.nih.gov/RxNav/search?searchBy=RXCUI&searchTerm=",
+    }
 
-    response = await api_client.get("/disease/normalize")
+    response = await api_client.get("/therapy/normalize")
     assert response.status_code == 422
 
 
@@ -62,16 +67,18 @@ async def test_normalize(api_client: AsyncClient):
 async def test_service_info(api_client: AsyncClient):
     response = await api_client.get("/service_info")
     assert response.status_code == 200
-    expected_version_pattern = r"\d\.\d\."  # at minimum, should be something like "0.1"
+    expected_version_pattern = (
+        r"\d+\.\d+\.?"  # at minimum, should be something like "0.1"
+    )
     response_json = response.json()
-    assert response_json["id"] == "org.genomicmedlab.disease_normalizer"
-    assert response_json["name"] == "disease_normalizer"
+    assert response_json["id"] == "org.genomicmedlab.thera_py"
+    assert response_json["name"] == "thera_py"
     assert response_json["type"]["group"] == "org.genomicmedlab"
-    assert response_json["type"]["artifact"] == "Disease Normalizer API"
+    assert response_json["type"]["artifact"] == "Thera-Py API"
     assert re.match(expected_version_pattern, response_json["type"]["version"])
     assert (
         response_json["description"]
-        == "Resolve ambiguous references and descriptions of human diseases to consistently-structured, normalized terms"
+        == "Resolve ambiguous references and descriptions of drugs and therapies to consistently-structured, normalized terms"
     )
     assert response_json["organization"] == {
         "name": "Genomic Medicine Lab at Nationwide Children's Hospital",
@@ -80,7 +87,7 @@ async def test_service_info(api_client: AsyncClient):
     assert response_json["contactUrl"] == "Alex.Wagner@nationwidechildrens.org"
     assert (
         response_json["documentationUrl"]
-        == "https://disease-normalizer.readthedocs.io/"
+        == "https://github.com/cancervariants/therapy-normalization"
     )
     assert datetime.fromisoformat(response_json["createdAt"])
     assert ServiceEnvironment(response_json["environment"])
