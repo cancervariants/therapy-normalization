72ad8946877c11c1f1102b13658f011c5249cdbd
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-07-09 21:12:57 -0400

    wtf

--- src/therapy/etl/update.py
+++ src/therapy/etl/update.py
@@ -63,7 +63,7 @@ def load_source(
 
     # used to get source class name from string
     try:
-        from therapy.etl import (
+        from therapy.etl import (  # noqa: PLC0415
             ChEMBL,
             ChemIDplus,
             DrugBank,
@@ -193,7 +193,7 @@ def update_normalized(
         processed_ids = db.get_all_concept_ids()
 
     try:
-        from therapy.etl.merge import Merge
+        from therapy.etl.merge import Merge  # noqa: PLC0415
     except ModuleNotFoundError as e:
         msg = f"Encountered ModuleNotFoundError attempting to import {e.name}. {_etl_dependency_help}"
         if not silent:
