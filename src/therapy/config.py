d112d6e8721ee8b70c55e93a00adbc272e8f70ca
James Stevenson <james.stevenson@nationwidechildrens.org>
2025-09-15 18:27:09 -0400

    fix env var again

--- src/therapy/config.py
+++ src/therapy/config.py
@@ -15,7 +15,7 @@ class Settings(BaseSettings):
     """
 
     model_config = SettingsConfigDict(
-        env_prefix="therapy_",
+        env_prefix="therapy_norm_",
         env_file=".env",
         env_file_encoding="utf-8",
         extra="ignore",
